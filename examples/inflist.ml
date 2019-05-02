05614f183b4145fddef1c3ef69c59cc5d1c4e63c
UTAGAWA Kiki <utagawakiki@gmail.com>
2019-05-03 06:06:26 +0900

    add some example codes

---
+++ examples/inflist.ml
@@ -0,0 +1,18 @@
+let rec take = fun n -> fun xs -> match xs with
+    [] -> []
+  | hd :: tl -> if n < 1 then []
+      else hd :: take (n + -1) tl
+in
+let tl = fun xs -> match xs with
+    [] -> []
+  | hd :: tl -> tl
+in
+let rec zipwith = fun f -> fun xs -> fun ys -> match xs with
+    [] -> []
+  | xhd :: xtl -> match ys with
+      [] -> []
+    | yhd :: ytl -> f xhd yhd :: zipwith f xtl ytl
+in
+let rec fib = 0 :: 1 :: zipwith (fun x -> fun y -> x + y) fib (tl fib) in
+take 20 fib
+;;
---
+++ examples/takeuchi.ml
@@ -0,0 +1,6 @@
+let rec tarai = fun x -> fun y -> fun z ->
+  if y < x then
+    tarai (tarai (x + -1) y z) (tarai (y + -1) z x) (tarai (z + -1) x y)
+  else y
+in tarai 100 30 0
+;;
