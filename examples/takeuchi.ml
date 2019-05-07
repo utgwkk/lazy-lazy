864176d19a64c29318019a86911fbc7a7388b15f
UTAGAWA Kiki <utagawakiki@gmail.com>
2019-05-07 13:15:59 +0900

    fix arguments for Takeuchi function

--- examples/takeuchi.ml
+++ examples/takeuchi.ml
@@ -2,5 +2,5 @@ let rec tarai = fun x -> fun y -> fun z ->
   if y < x then
     tarai (tarai (x + -1) y z) (tarai (y + -1) z x) (tarai (z + -1) x y)
   else y
-in tarai 100 30 0
+in tarai 12 6 0
 ;;
